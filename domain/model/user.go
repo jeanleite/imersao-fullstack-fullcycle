d205c92d41af8cb9c72a5e25bb638b0b6422c8bd
Jean Leite <jean.michel.pc@gmail.com>
2021-02-04 17:48:09 -0300

    create user model

---
+++ domain/model/user.go
@@ -0,0 +1,48 @@
+package model
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type UserRepositoryInterface interface {
+	Register(user *User) error
+	Save(user *User) error
+	Find(id string) (*User, error)
+}
+
+type User struct {
+	Base  `valid:"required"`
+	Name  string `json:"name" valid:"notnull"`
+	Email string `json:"email" valid:"notnull"`
+}
+
+func (user *User) isValid() error {
+	_, err := govalidator.ValidateStruct(user)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewUser(name string, email string) (*User, error) {
+	user := User{
+		Name:  name,
+		Email: email,
+	}
+
+	user.ID = uuid.NewV4().String()
+	user.CreatedAt = time.Now()
+
+	err := user.isValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
